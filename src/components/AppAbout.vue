cd7afbfd6870064fd7bbf3209818b6c0d2f7b094
Per Steen Olsen <persteen22@hotmail.com>
2021-05-11 14:53:11 +0200

    Adjusting the layout of the About page to be more readable

--- src/components/AppAbout.vue
+++ src/components/AppAbout.vue
@@ -3,51 +3,52 @@
    
                 <h1>Description</h1>
   
-                This Vue single page application was made with a Webpack 5 configuration
+                - This Vue single page application was made with a Webpack 5 configuration
                 <br/> <br/>
 				
                 <b>Last Updated</b>  <br />
-                27-04-2021 <br /> <br />
+                - 11-05-2021 <br /> <br />
                 				
                 <b>Main functionality</b> <br />
                 				                 
-			    Showing some of the power of Webpack <br />
-                Showing routing by Vue.js <br />
-				Support for IE 11 <br />
-				Custom 404 Error Page. Click the Secret menu link for a demo <br />
-			    Serving static assets like images, styles and fonts <br />
-			    Simulate GET, POST, PUT and DELETE towards a test Web API <br /> <br />
+			    - Showing some of the power of Webpack <br />
+				- Using Hot Reload / HMR by Webpack Dev Server <br />
+                - Showing routing by Vue.js <br />
+				- Support for IE 11 <br />
+				- Custom 404 Error Page. Click the Secret menu link for a demo <br />
+			    - Serving static assets like images, styles and fonts <br />
+			    - Simulate GET, POST, PUT and DELETE towards a test Web API <br /> <br />
                
 			   
                 <b>Module Bundler</b> <br />
                 
-                Manuel configuration of the Webpack 5 from scratch <br /> <br />
+                - Manuel configuration of the Webpack 5 from scratch <br /> <br />
 				
                 
 				<b>The Vue single page application - The frontend</b> <br />
             
                
-                Mainly Vue.js<br />
-                Bootstrap 4 by CDN for the responsive design <br /> <br />
+                - Mainly Vue.js<br />
+                - Bootstrap 4 by CDN for the responsive design <br /> <br />
 
             
                 <b>The Web API - The backend</b> <br />
  
-                The Web API is jsonplaceholder used for testing frontends <br /> <br />
+                - The Web API is jsonplaceholder used for testing frontends <br /> <br />
 
                 				
                 <b>Security</b> <br />
              
-                The only security is the use of HTTPS because it is a demo only <br /> <br />
+                - The only security is the use of HTTPS because it is a demo only <br /> <br />
 				
 				
 				<b>Hosting</b> <br />
-                The application is hosted at a traditionel Webhotel <br /> <br />
+                - The application is hosted at a traditionel Webhotel <br /> <br />
              
 
                 <b>Text Editor</b> <br />
              
-			    Visual Studio Code <br />
+			    - Visual Studio Code <br />
             			 
 	
   </div>
